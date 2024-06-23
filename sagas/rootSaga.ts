9ae207605b68b9b04a6b30e64315890efc77d696
benz salary hero <benzson.v@gmail.com>
2024-06-23 23:33:44 +0700

    Delete unnecessary content

--- sagas/rootSaga.ts
+++ sagas/rootSaga.ts
@@ -1,11 +1,7 @@
 import { all } from 'redux-saga/effects';
 
 import { watchFetchLeaderboard } from '../watchers/leaderboardWatcher';
-// import { watchSearchUser } from '../watchers/userWatcher';
 
 export default function* rootSaga() {
-  yield all([
-    watchFetchLeaderboard(),
-    // watchSearchUser(),
-  ]);
+  yield all([watchFetchLeaderboard()]);
 }
